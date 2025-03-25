a0b0ef9fa781a3427763d840b9419fd15c7fcb49
romain <romain.meunier@le-campus-numerique.fr>
2025-03-25 12:21:20 +0100

    modif self et ajout voir  ano colonnes

--- Scripts/N1.py
+++ Scripts/N1.py
@@ -12,86 +12,80 @@ import anonypy
 
 class Preprocessing:
     def __init__(self,dataset,parameters):
-        self.dataset=dataset.copy()
-        self._dataset_original=dataset.copy()
+        self.__dataset=dataset.copy()
+        self.dataset_original=dataset.copy()
         self.parameters=parameters
         self.run_preprocess()
-
 
     def run_preprocess(self):
         self.stop=False
 
         # anonymise specific columns in dataset
         if not self.stop and 'anonymiser' in self.parameters.keys():
-            self.dataset=self.anonymiser(self.dataset,self.parameters['anonymiser']['k'],self.parameters['anonymiser']['feature_columns'])
-            self._dataset_anonymized=self.dataset.copy()
+            try:
+                sensitive_columns=self.parameters['anonymiser']['sensitive_columns']
+            except:
+                sensitive_columns='ano_index'
+            self.anonymiser(self.parameters['anonymiser']['k'],self.parameters['anonymiser']['feature_columns'],sensitive_columns)
+            self.dataset_anonymized=self.__dataset.copy()
 
         # generate dummies for multicategorical_features
+        if not self.stop:
+            if 'multicategorical_features' in self.parameters['features'].keys():
+                self.get_dummies(self.parameters['features']['multicategorical_features'])
+                self.dataset_dummies=self.__dataset.copy()
+            
         # split dataset in X and y 
         if not self.stop:
-            features=self.parameters['features']
-            if 'multicategorical_features' in features.keys():
-                self.dataset=self.get_dummies(self.dataset,features['multicategorical_features'])
-                self._dataset_dummies=self.dataset.copy()
-            self._X,self._y=self.split_X_y(self.dataset,features['target'])
-
-        # run train test _split
-        tts_params=self.parameters['tts_params']
-        if not self.stop:
-            self._X_train, self._X_test, self._y_train, self._y_test = self.train_test_split(
-                self._X,self._y,tts_params['random_state'],tts_params['test_size']
-                )
+            self.split_X_y(self.parameters['features']['target'])
 
         # run train test _split
         if not self.stop:
-            self._X_train_scaled, self._X_test_scaled = self.scaler(self._X_train,self._X_test,features['continuous_features'])
+            self.train_test_split(self.parameters['tts_params']['random_state'],self.parameters['tts_params']['test_size'])
+
+        # run scaler
+        if not self.stop:
+            self.scaler(self.parameters['features']['continuous_features'])
         
         # output
         if not self.stop:
-            self.preprocess_dataset={
+            self.dataset_preprocessed={
                 'X_train': self._X_train_scaled, 
                 'X_test' : self._X_test_scaled,
                 'y_train': self._y_train, 
                 'y_test': self._y_test}
     
-    def get_dummies(self,dataset,multicategorical_features):
-        return pd.get_dummies(dataset,columns=multicategorical_features,dtype=int)
+    def get_dummies(self,multicategorical_features):
+        # Transpose chacune des colonnes multicatégories en dummies
+        self.__dataset=pd.get_dummies(self.__dataset,columns=multicategorical_features,dtype=int)
 
-    def plot_hist(self,dataset):
-        rcParams['figure.figsize'] = 16,12
-        dataset.hist(bins=20)
-        plt.show()
+    def split_X_y(self,target):
+        # Split le dataset en X et y datasets
+        self._y = self.__dataset[target]
+        self._X = self.__dataset.drop(target,axis=1)
 
-    def split_X_y(self,dataset,target):
-        y = dataset[target]
-        X = dataset.drop(target,axis=1)
-        return X,y
-    
-    def train_test_split(self,X,y,random_state=41, test_size=0.3,stratify_value=None):
+    def train_test_split(self,random_state=41, test_size=0.3,stratify_value=None):
+        # Definit les variables locales à partir du fichier de paramètres
         if stratify_value == None : 
-            X_train, X_test, y_train, y_test = train_test_split(X, y, random_state = random_state,test_size = test_size)
-        return X_train, X_test, y_train, y_test
+            self._X_train, self._X_test, self._y_train, self._y_test = train_test_split(
+                self._X, self._y, random_state = random_state,test_size = test_size)
     
-    def scaler(self,X_train,X_test,continuous_features):
+    def scaler(self,continuous_features):
         #Copie les données dans un nouveau jeu de données
-        X_train_scaled=X_train.copy()
-        X_test_scaled=X_test.copy()
+        self._X_train_scaled=self._X_train.copy()
+        self._X_test_scaled=self._X_test.copy()
 
         #Normalise sur les valeurs continues avec standard scale
         scaler=StandardScaler()
-        X_train_scaled[continuous_features]=scaler.fit_transform(X_train[continuous_features])
-        X_test_scaled[continuous_features]=scaler.transform(X_test[continuous_features])
-        # print("X_train size = {0}; X_train_scaled size = {1}\nX_test size = {2}; X_test_scaled size = {3}".format(X_train.size,X_train_scaled.size,X_test.size,X_test_scaled.size))
-
-        return X_train_scaled,X_test_scaled
+        self._X_train_scaled[continuous_features]=scaler.fit_transform(self._X_train[continuous_features])
+        self._X_test_scaled[continuous_features]=scaler.transform(self._X_test[continuous_features])
     
-    def anonymiser(self,dataset,k,feature_columns,sensitive_columns='ano_index'):
+    def anonymiser(self,k,feature_columns,sensitive_columns):
         # Copie les colonnes à anonymiser dans un df_local
-        df_ano=dataset[feature_columns].copy()
+        df_ano=self.__dataset[feature_columns].copy()
 
         #Crée une colonne 'index' de la longeur du df_ano
-        if sensitive_columns=='ano_index':
-            df_ano['ano_index']=range(len(dataset))
+        df_ano['ano_index']=range(len(df_ano))
 
         # Genere l'anonymisation des colonnes 'features_columns'
         p=anonypy.Preserver(df_ano,feature_columns,sensitive_columns)
@@ -99,10 +93,11 @@ class Preprocessing:
         dfn = pd.DataFrame(rows)
 
         # remplace les colonnes anonymisées dans le dataset d'origine
-        # ajoute les colonnes anonymisées dans la catégorie 'multicategorical_features' et les retire de la catégorie 'continuous_features'
+        self.anonymized_columns={}
         for column in feature_columns:
-            dataset[column]=dfn[column].astype('str')
-            dataset[column]=dataset[column].astype('category')
+            self.__dataset[column]=dfn[column].astype('str')
+            self.__dataset[column]=self.__dataset[column].astype('category')
+            # ajoute les colonnes anonymisées dans la catégorie 'multicategorical_features' et les retire de la catégorie 'continuous_features'
             try :
                 self.parameters['features']['multicategorical_features'].append(column)
             except:
@@ -111,31 +106,37 @@ class Preprocessing:
                 self.parameters['features']['continuous_features'].remove(column)
             except:
                 None
-        return dataset
+            # ajoute les colonnes anonymisées et le nombre de valeur par catégories pour output
+            self.anonymized_columns[column]=self.__dataset[column].value_counts().sort_index().to_frame()
+
+    def plot_hist(self,dataset):
+        rcParams['figure.figsize'] = 16,12
+        dataset.hist(bins=20)
+        plt.show()
 
 
 class Classification:
     def __init__(self,dataset,model):
         self.model=model
-        self.X_train=dataset['X_train']
-        self.X_test=dataset['X_test']
-        self.y_train=dataset['y_train']
-        self.y_test=dataset['y_test']
+        self._X_train=dataset['X_train']
+        self._X_test=dataset['X_test']
+        self._y_train=dataset['y_train']
+        self._y_test=dataset['y_test']
         
         self.model_fit_and_predict()
         self.metrics()
 
     def model_fit_and_predict(self):
-        self.model.fit(self.X_train,self.y_train)
-        self.y_test_pred = self.model.predict(self.X_test)
+        self.model.fit(self._X_train,self._y_train)
+        self.y_test_pred = self.model.predict(self._X_test)
     
     def metrics(self):
         self.metrics_results={}
-        self.metrics_results['conf_matrix']= confusion_matrix(self.y_test,self.y_test_pred)
-        self.metrics_results['precision']=precision_score(self.y_test,self.y_test_pred)
-        self.metrics_results['recall']=recall_score(self.y_test,self.y_test_pred)
-        self.metrics_results['accuracy']=accuracy_score(self.y_test,self.y_test_pred)
-        self.metrics_results['f1']=f1_score(self.y_test,self.y_test_pred)
+        self.metrics_results['conf_matrix']= confusion_matrix(self._y_test,self.y_test_pred)
+        self.metrics_results['precision']=precision_score(self._y_test,self.y_test_pred)
+        self.metrics_results['recall']=recall_score(self._y_test,self.y_test_pred)
+        self.metrics_results['accuracy']=accuracy_score(self._y_test,self.y_test_pred)
+        self.metrics_results['f1']=f1_score(self._y_test,self.y_test_pred)
         return self.metrics_results
     
     def results(self):
@@ -143,7 +144,7 @@ class Classification:
     
 class GridSearch:
     def __init__(self,dataset,gs_parameters,model):
-        self.dataset=dataset
+        self.dataset=dataset.copy()
         self.gs_parameters=gs_parameters
         self.model=model
         self.run_gs()
@@ -153,7 +154,7 @@ class GridSearch:
         for key in self.gs_parameters:
             pp=Preprocessing(self.dataset,self.gs_parameters[key])
             knn = KNeighborsClassifier(n_neighbors=15,n_jobs=-1)
-            model=Classification(pp.preprocess_dataset,knn)
+            model=Classification(pp.dataset_preprocessed,knn)
             self.gs_results[str(key)]=model.metrics_results
             self.df=pd.DataFrame(self.gs_results).T
 
